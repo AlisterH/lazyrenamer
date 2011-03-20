176a8d669b5a350f493a6c25c0a8222258c01827
hoodbild@maxnet.co.nz <hoodbild@maxnet.co.nz@d5de5775-149c-813f-6507-2512062d85ba>
2011-03-20 09:04:06 +0000

    feature: add copy button and increase window size

--- LazyRenamer/Form1.Designer.vb
+++ LazyRenamer/Form1.Designer.vb
@@ -21,30 +21,21 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.btnRename = New System.Windows.Forms.Button
-        Me.btnHiddenFocus = New System.Windows.Forms.Button
         Me.lblFile = New System.Windows.Forms.Label
         Me.txtNewName = New System.Windows.Forms.TextBox
+        Me.btnCopy = New System.Windows.Forms.Button
         Me.SuspendLayout()
         '
         'btnRename
         '
         Me.btnRename.Enabled = False
         Me.btnRename.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnRename.Location = New System.Drawing.Point(114, 45)
+        Me.btnRename.Location = New System.Drawing.Point(115, 83)
         Me.btnRename.Name = "btnRename"
         Me.btnRename.Size = New System.Drawing.Size(82, 24)
-        Me.btnRename.TabIndex = 16
+        Me.btnRename.TabIndex = 17
         Me.btnRename.Text = "Rename"
         Me.btnRename.UseVisualStyleBackColor = True
-        '
-        'btnHiddenFocus
-        '
-        Me.btnHiddenFocus.Location = New System.Drawing.Point(114, 88)
-        Me.btnHiddenFocus.Name = "btnHiddenFocus"
-        Me.btnHiddenFocus.Size = New System.Drawing.Size(82, 24)
-        Me.btnHiddenFocus.TabIndex = 14
-        Me.btnHiddenFocus.Text = "HiddenFocus"
-        Me.btnHiddenFocus.UseVisualStyleBackColor = True
         '
         'lblFile
         '
@@ -53,33 +44,46 @@ Partial Class Form1
         Me.lblFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.lblFile.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblFile.Location = New System.Drawing.Point(0, 1)
+        Me.lblFile.MinimumSize = New System.Drawing.Size(196, 59)
         Me.lblFile.Name = "lblFile"
-        Me.lblFile.Size = New System.Drawing.Size(196, 42)
-        Me.lblFile.TabIndex = 17
+        Me.lblFile.Size = New System.Drawing.Size(196, 59)
+        Me.lblFile.TabIndex = 0
         Me.lblFile.Text = "<Drag and drop associated file here>"
         Me.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'txtNewName
         '
         Me.txtNewName.Enabled = False
-        Me.txtNewName.Location = New System.Drawing.Point(0, 47)
+        Me.txtNewName.Location = New System.Drawing.Point(0, 62)
         Me.txtNewName.Name = "txtNewName"
-        Me.txtNewName.Size = New System.Drawing.Size(110, 20)
+        Me.txtNewName.Size = New System.Drawing.Size(196, 20)
         Me.txtNewName.TabIndex = 15
         Me.txtNewName.Text = "Enter new name"
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Enabled = False
+        Me.btnCopy.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCopy.Location = New System.Drawing.Point(0, 83)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(82, 24)
+        Me.btnCopy.TabIndex = 16
+        Me.btnCopy.Text = "Copy"
+        Me.btnCopy.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(196, 69)
+        Me.ClientSize = New System.Drawing.Size(196, 107)
+        Me.Controls.Add(Me.btnCopy)
         Me.Controls.Add(Me.btnRename)
-        Me.Controls.Add(Me.btnHiddenFocus)
         Me.Controls.Add(Me.lblFile)
         Me.Controls.Add(Me.txtNewName)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(202, 109)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "LazyRenamer"
@@ -89,8 +93,8 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents btnRename As System.Windows.Forms.Button
-    Friend WithEvents btnHiddenFocus As System.Windows.Forms.Button
     Friend WithEvents lblFile As System.Windows.Forms.Label
     Friend WithEvents txtNewName As System.Windows.Forms.TextBox
+    Friend WithEvents btnCopy As System.Windows.Forms.Button
 
 End Class
--- LazyRenamer/Form1.vb
+++ LazyRenamer/Form1.vb
@@ -46,6 +46,7 @@ Public Class Form1
         lblFile.Text = FileDrop
         txtNewName.Text = FileName
         btnRename.Enabled = False
+        btnCopy.Enabled = False
     End Sub
     Private Sub txtNewName_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtNewName.KeyPress
         'Prevents user from entering illegal file name characters in textbox
@@ -62,12 +63,16 @@ Public Class Form1
     End Sub
     Private Sub txtNewName_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtNewName.KeyUp
         btnRename.Enabled = True
+        btnCopy.Enabled = True
         If Trim(txtNewName.Text) = "" Then btnRename.Enabled = False
-        'Disables the Rename button if any file with that name and any extension already exists
+        'Disables the Rename and Copy buttons if any file with that name and any extension already exists
         For Each foundFile As String In My.Computer.FileSystem.GetFiles(FilePath)
             FilesInDir = VB.Right(foundFile, VB.Len(foundFile) - InStrRev(foundFile, "\"))
             If InStr(FilesInDir, ".") <> 0 Then FilesInDir = VB.Left(FilesInDir, InStr(FilesInDir, ".") - 1)
-            If LCase(txtNewName.Text) = LCase(FilesInDir) Then btnRename.Enabled = False
+            If LCase(txtNewName.Text) = LCase(FilesInDir) Then
+                btnRename.Enabled = False
+                btnCopy.Enabled = False
+            End If
         Next
     End Sub
     Private Sub btnRename_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRename.Click
@@ -97,15 +102,83 @@ Public Class Form1
         'Assigns the new name to the FileDrop variable
         FileDrop = FilePath & txtNewName.Text & FileExtension
         btnRename.Enabled = False
+        btnCopy.Enabled = False
         GoTo 110
 100:    'Jumps here when "On Error" occurs
         lblFile.Text = "One or more of the associated files appears to be in use by another program. Aborting."
         lblFile.BackColor = Color.LavenderBlush
-        btnHiddenFocus.Focus()
 110:    'Assigns the new names (removes #tmp suffix) or restores names when error occurs
         For Each foundFile As String In My.Computer.FileSystem.GetFiles(FilePath)
             If VB.Right(foundFile, 4) = "#tmp" Then Rename(foundFile, VB.Left(foundFile, VB.Len(foundFile) - 4))
         Next
+        'Updates the Layer Name value in MapWindow layer properties files
+        MwsrFileIn = FilePath & txtNewName.Text & ".mwsr"
+        If File.Exists(MwsrFileIn) Then
+            foundFileReadOnly = My.Computer.FileSystem.GetFileInfo(MwsrFileIn)
+            If foundFileReadOnly.IsReadOnly = False Then
+                MwsrFileOut = FilePath & FileNameTxtbox & ".mwsr2"
+                FileOpen(1, MwsrFileIn, OpenMode.Input)
+                FileOpen(2, MwsrFileOut, OpenMode.Output)
+                MwsrLabelName = "Layer Name=" & ChrW(34) & FileName & ChrW(34)
+                MwsrLabelNameNew = "Layer Name=" & ChrW(34) & txtNewName.Text & ChrW(34)
+                Do
+                    MwsrLine = LineInput(1)
+                    If InStr(MwsrLine, MwsrLabelName) <> 0 Then MwsrLine = Replace(MwsrLine, MwsrLabelName, MwsrLabelNameNew)
+                    PrintLine(2, MwsrLine)
+                Loop Until VB.EOF(1) = True
+                FileClose(1, 2)
+                File.Delete(MwsrFileIn)
+                Rename(MwsrFileOut, VB.Left(MwsrFileOut, VB.Len(MwsrFileOut) - 1))
+            End If
+        End If
+        FileName = lblFile.Text
+        If File.Exists(MwsrFileOut) = True Then File.Delete(MwsrFileIn)
+        If lblFile.BackColor <> Color.LavenderBlush Then txtNewName.Focus()
+        'Updates the file label with the new file name
+        If lblFile.Text <> "One or more of the associated files appears to be in use by another program. Aborting." Then
+            lblFile.BackColor = Color.WhiteSmoke
+            'Updates lblfile.text with the new name ...
+            If FileNameDot <> FileName Then
+                lblFile.Text = FilePath & txtNewName.Text & VB.Mid(FileNameDot, InStr(FileNameDot, "."), VB.Len(FileNameDot))
+            Else : lblFile.Text = FilePath & txtNewName.Text & FileExtension
+            End If
+        End If
+    End Sub
+    'We really should share code with btnRename_Click() instead of just copying the code ;)
+    Private Sub btnCopy_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCopy.Click
+        'Copy associated files
+        FileName = VB.Right(FileDrop, VB.Len(FileDrop) - InStrRev(FileDrop, "\"))
+        If InStr(FileName, ".") <> 0 Then FileName = VB.Left(FileName, InStr(FileName, ".") - 1)
+        FileNameTxtbox = VB.Right(txtNewName.Text, VB.Len(txtNewName.Text) - InStrRev(txtNewName.Text, "\"))
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(FilePath)
+            foundFile = VB.Right(foundFile, VB.Len(foundFile) - InStrRev(foundFile, "\"))
+            If InStr(foundFile, ".") <> 0 Then FilesInDir = VB.Left(foundFile, InStr(foundFile, ".") - 1)
+            'foundFile is a full path (it goes through all the files in the folder)
+            'FilesInDir is a filename without extensions (it goes through all the files in the folder)
+            If FileNameTxtbox <> FileName Then
+                If LCase(FilesInDir) = LCase(FileName) Then
+                    If InStr(foundFile, ".") <> 0 Then
+                        foundFileExtension = VB.Right(foundFile, VB.Len(foundFile) - InStrRev(foundFile, ".") + 1)
+                        On Error GoTo 100   'Shouldn't get an error copying
+                        If InStr(foundFile, ".") = InStrRev(foundFile, ".") Then
+                            My.Computer.FileSystem.CopyFile(FilePath & foundFile, FilePath & FileNameTxtbox & foundFileExtension)
+                            'Renames files with more-than-one dot, e.g. Land.shp.xml in a Land.*-series
+                        Else : My.Computer.FileSystem.CopyFile(FilePath & foundFile, FilePath & FileNameTxtbox & VB.Mid(foundFile, InStr(foundFile, "."), (InStrRev(foundFile, ".") - InStr(foundFile, "."))) & foundFileExtension)
+                        End If
+                    End If
+                End If
+            End If
+        Next
+        lblFile.BackColor = Color.WhiteSmoke
+        'Assigns the new name to the FileDrop variable
+        FileDrop = FilePath & txtNewName.Text & FileExtension
+        btnRename.Enabled = False
+        btnCopy.Enabled = False
+        GoTo 110
+100:    'Jumps here when "On Error" occurs
+        lblFile.Text = "Aborting. This seems to be a bug!"
+        lblFile.BackColor = Color.LavenderBlush
+110:    'Hmmm.  What happens if an error occurs while copying?
         'Updates the Layer Name value in MapWindow layer properties files
         MwsrFileIn = FilePath & txtNewName.Text & ".mwsr"
         If File.Exists(MwsrFileIn) Then
