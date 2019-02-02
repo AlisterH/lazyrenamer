d38161789ffc25e2a6cdb921080e53a45832e745
AlisterH <alister.hood@gmail.com>
2019-02-03 03:53:04 +1300

    update: version number and copyright for v1.3

--- LazyRenamer/My Project/AssemblyInfo.vb
+++ LazyRenamer/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Quickly rename or copy a group of associated files")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("LazyRenamer")> 
-<Assembly: AssemblyCopyright("Copyright ©  2008, 2011, 2012")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008, 2011, 2012, 2019")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.0")> 
-<Assembly: AssemblyFileVersion("1.2.0.0")> 
+<Assembly: AssemblyVersion("1.3.0.0")> 
+<Assembly: AssemblyFileVersion("1.3.0.0")> 
